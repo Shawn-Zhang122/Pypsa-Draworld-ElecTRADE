7b795e416ec0192520290cdb28eac4ad32bc00a7
Shuwei-zhang <shuwei.zh@gmail.com>
2026-02-17 11:26:13 +0100

    ElecTRADE: add index.html

---
+++ run_daily.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# load conda
+source /Users/zhang/opt/anaconda3/etc/profile.d/conda.sh
+
+# activate environment
+conda activate pypsa-china
+
+# go to project root
+cd /Users/zhang/Resource/China-ElecTrade-APP/Pypsa-Draworld-ElecTRADE
+
+# set Gurobi license (relative to project root)
+export GRB_LICENSE_FILE=$PWD/solver/gurobi.lic
+
+# run daily model
+python model/run_daily.py
+
+# push results only if changed
+git add data/
+git diff --cached --quiet || git commit -m "daily update $(date +%F)"
+git push
